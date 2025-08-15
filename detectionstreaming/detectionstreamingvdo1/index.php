76df595900cf64e4ff87c5fca6a5b3d2f4bc2e71
werayut <l2omance1992@gmail.com>
2025-08-15 15:54:01 +0700

    no message

--- detectionstreaming/detectionstreamingvdo1/index.php
+++ detectionstreaming/detectionstreamingvdo1/index.php
@@ -1,47 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <title>Network Link Co., Ltd.</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://vjs.zencdn.net/8.10.0/video-js.css" rel="stylesheet" />
-  <style>
-    .video-js {
-      max-width: 100%;
-      height: auto;
-    }
-    html, body {
-      overflow: hidden;
-      margin: 0;
-      background: #000;
-    }
-  </style>
+    <meta charset=utf-8 />
+    <title>Network Link Co., Ltd.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <link href="https://vjs.zencdn.net/8.10.0/video-js.css" rel="stylesheet" />
+    <script src="../../css/video.js"></script>
+	<script src="../../css/hls.js"></script>
+    <style>
+        video {
+            width: 100%;
+            height: auto;
+        }
+        html,
+        body {
+            overflow-y: hidden;
+            overflow-x: hidden;
+        }
+        @media only screen and (max-width: 600px) {
+            #km_19 {
+                width: 350px;
+                height: 350px;
+            }
+        }
+        @media only screen and (min-width: 601px) {
+            #km_19 {
+                width: 790px;
+                height: 650px;
+            }
+        }
+    </style>
 </head>
 <body>
-  <video
-    id="km_19"
-    class="video-js vjs-default-skin vjs-big-play-centered"
-    controls
-    autoplay
-    muted
-    playsinline
-    data-setup='{"fluid": true}'
-  >
-    <source src="st.m3u8?v=<?=time()?>" type="application/x-mpegURL" />
-  </video>
-
-  <script src="https://vjs.zencdn.net/8.10.0/video.min.js"></script>
-
-  <script>
-    var player = videojs('km_19');
-    player.ready(function() {
-      player.play();
-    });
-
-    player.on('error', function() {
-      console.error('VideoJS Error:', player.error());
-      alert("เกิดข้อผิดพลาดในการโหลดสตรีม");
-    });
-  </script>
+    <html>
+    <body>
+        <video id="video" muted playsinline controls autoplay ></video>
+        <script>
+            if (Hls.isSupported()) {
+                var video = document.getElementById('video');
+                var hls = new Hls();
+                hls.loadSource('st.m3u8');
+                hls.attachMedia(video);
+                hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                    video.play();
+                });
+            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                video.src = 'st.m3u8';
+                video.addEventListener('canplay', function() {
+                    video.play();
+                });
+            }
+        </script>
+    </body>
 </body>
 </html>
