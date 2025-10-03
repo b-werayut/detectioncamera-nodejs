f16ad1cd394f408357e0b1a7275fd6474de29205
MSI\Werayut Jaisue <l2omance1992@gmail.com>
2025-10-03 17:17:32 +0700

    no message

--- camera/routes/directory.js
+++ camera/routes/directory.js
@@ -2,8 +2,9 @@ const express = require('express')
 const { GetLogsData } = require('../controllers/getLogs')
 const { insertPowerLogs, getUserIDCustomer, getUserIDCustomerexternal, getDataFlutter } = require('../controllers/DatabaseManage')
 const { delayEventFunct } = require('../middlefunct/DelaySendLineFunct')
-const { manageDirectory, deleteOldDir } = require('../controllers/directory')
+const { manageDirectory, deleteOldDir, delDir } = require('../controllers/directory')
 const { getCameraStat } = require('../controllers/getCameraStat')
+// const { pushAndroidNotificationPower } = require('../controllers/Firebase-Services')
 const router = express.Router()
 
 router.get('/getuserid/', getUserIDCustomerexternal)
@@ -12,8 +13,13 @@ router.get('/directory/:camname', delayEventFunct, manageDirectory)
 // router.get('/directory/', manageDirectory)
 router.get('/getlogs/:params', GetLogsData)
 router.get('/getcamerastat', getCameraStat)
+router.get('/deldir', delDir)
 // router.delete('/testdelfolder', deleteOldDir)
 
 router.post('/powerlogs/:point/:status/:val/:timestamp/', insertPowerLogs)
+
+//Api Mobile platform
+// router.post('/pushnotification/:point/:val/:timestamp', pushAndroidNotificationPower)
+// router.delete('/deletedataflutter/:id', deleteDataFlutter);
 
 module.exports = router
